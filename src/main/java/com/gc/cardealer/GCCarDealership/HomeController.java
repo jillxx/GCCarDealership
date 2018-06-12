4537c138efabc86c3eb76e098a90c0c7bc63bb83
John <johnaoraha@gmail.com>
2018-06-12 11:33:09 -0400

    updated home controller

--- src/main/java/com/gc/cardealer/GCCarDealership/HomeController.java
+++ src/main/java/com/gc/cardealer/GCCarDealership/HomeController.java
@@ -1,8 +1,13 @@
 package com.gc.cardealer.GCCarDealership;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.gc.cardealer.GCCarDealership.model.Car;
 
 @Controller
 public class HomeController {
@@ -11,5 +16,14 @@ public class HomeController {
 	public ModelAndView index() {
 		return new ModelAndView("index");
 	}
+	
+	@RequestMapping("makeresults")
+	public ModelAndView makeresults(@RequestParam("make") String m) {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Car make = restTemplate.getForObject("http://localhost:8080/make?make="+ m, Car.class);
+		return new ModelAndView("makeresults", "car", make);
+	}
+
 
 }
